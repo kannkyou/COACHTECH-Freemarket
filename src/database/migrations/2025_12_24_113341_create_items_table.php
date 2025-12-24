57cd3eda61b057ba9791e596d1f1ce4ecd7409f2
kannkyou <furusato.takuya@gmail.com>
2025-12-24 15:05:13 +0900

    first commit

---
+++ src/database/migrations/2025_12_24_113341_create_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('seller_id')->constrained('users')->cascadeOnDelete();
+            $table->tinyInteger('status'); 
+            $table->string('title');
+            $table->string('brand_name');
+            $table->integer('price');
+            $table->text('description');
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
+            $table->tinyInteger('condition');
+            $table->text('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
---
+++ src/database/migrations/2025_12_24_113442_create_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('buyer_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('seller_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('item_id')->constrained('items')->unique()->cascadeOnDelete();
+            $table->tinyInteger('status');
+            $table->integer('item_price');
+            $table->tinyInteger('payment_method');
+            $table->string('shipping_postal_code');
+            $table->string('shipping_address');
+            $table->string('shipping_building')->nullable();
+            $table->timestamp('paid_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
